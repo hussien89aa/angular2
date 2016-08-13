1fe0c4bc6b86fed2f9b75615c3741e3f0635c17a
hussienalrubaye <hussien89aa@yahoo.com>
2016-08-13 11:41:28 -0400

    first

---
+++ process/typescript/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Welcome to Angualr 2</h1> <img src="images/angular2.jpg" /> '
+})
+
+export class AppComponent {
+
+}
